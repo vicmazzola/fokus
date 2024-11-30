098486b50bf7b275eb629f2df992f54ab90ff1ea
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-30 18:34:27 -0300

    change img btn

--- app/script.js
+++ app/script.js
@@ -4,7 +4,7 @@ const btnShort = document.querySelector('.app__card-button--short');
 const btnLong = document.querySelector('.app__card-button--long');
 const timer = document.querySelector('#timer');
 const banner = document.querySelector('.app__image');
-const title = document.querySelector('.app__title')
+const title = document.querySelector('.app__title');
 
 const focusDuration = 1500;
 const shortBreakDuration = 300;
@@ -12,21 +12,51 @@ const longBreakDuration = 900;
 
 
 btnFocus.addEventListener('click', () => {
-    html.setAttribute('data-context', 'focus')
-    banner.setAttribute('src', '/img/foco.png')
+    changeContext('focus')
 })
 
 btnShort.addEventListener('click', () => {
-    html.setAttribute('data-context', 'short-break')
-    banner.setAttribute('src', '/img/descanso-curto.png')
+    changeContext('short-break')
+
 
 })
 
 btnLong.addEventListener('click', () => {
-    html.setAttribute('data-context', 'long-break')
-    banner.setAttribute('src', '/img/descanso-longo.png')
+    changeContext('long-break')
+
 
 })
+
+function changeContext(context) {
+    const imagePath = `img/${context}.png`;
+    console.log("Image path:", imagePath); // Verifica o caminho no console
+    banner.setAttribute('src', imagePath);
+
+
+    html.setAttribute('data-context', context)
+    banner.setAttribute('src', `img/${context}.png`);
+    switch (context) {
+        case 'focus':
+            title.innerHTML =
+                `Optimize your productivity,<br>
+            <strong class="app__title-strong">dive into what matters.</strong>`
+
+            break;
+
+        case 'short-break':
+            title.innerHTML =
+                `How about take a break? 
+                <strong class="app__title-strong">Take a short break!</strong>`
+            break;
+        case 'long-break':
+            title.innerHTML =
+                `Time to rest! 
+                    <strong class="app__title-strong">Make a long break!</strong>`
+            break;
+        default:
+            break;
+    }
+}
 
 timer.addEventListener('click', () => {
 
